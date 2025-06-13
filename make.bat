e07c8bb36076224aa1102e707507addadb13d5c8
zh1peng <frueddy@gmail.com>
2025-06-13 09:48:11 +0800

    rm docs

--- make.bat
+++ make.bat
@@ -1,32 +1,37 @@
 @ECHO OFF
-
 pushd %~dp0
 
-REM Command file for Sphinx documentation
+REM Command file for building Sphinx documentation on Windows
 
-if "%SPHINXBUILD%" == "" (
-	set SPHINXBUILD=sphinx-build
+REM Set up defaults
+IF "%SPHINXBUILD%"=="" SET SPHINXBUILD=sphinx-build
+SET SOURCEDIR=source
+SET BUILDDIR=docs
+
+REM Check if sphinx-build exists
+%SPHINXBUILD% >NUL 2>&1
+IF ERRORLEVEL 9009 (
+    ECHO.
+    ECHO The 'sphinx-build' command was not found. Make sure you have Sphinx
+    ECHO installed, then set the SPHINXBUILD environment variable to point
+    ECHO to the full path of the 'sphinx-build' executable. Alternatively you
+    ECHO may add the Sphinx directory to PATH.
+    ECHO.
+    ECHO If you don't have Sphinx installed, grab it from:
+    ECHO https://www.sphinx-doc.org/
+    EXIT /B 1
 )
-set SOURCEDIR=source
-set BUILDDIR=.
 
-%SPHINXBUILD% >NUL 2>NUL
-if errorlevel 9009 (
-	echo.
-	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
-	echo.installed, then set the SPHINXBUILD environment variable to point
-	echo.to the full path of the 'sphinx-build' executable. Alternatively you
-	echo.may add the Sphinx directory to PATH.
-	echo.
-	echo.If you don't have Sphinx installed, grab it from
-	echo.https://www.sphinx-doc.org/
-	exit /b 1
+REM If no argument is given, show help
+IF "%1"=="" GOTO help
+
+REM Run the sphinx-build target
+IF "%1"=="html" (
+    %SPHINXBUILD% -b html %SOURCEDIR% %BUILDDIR%
+) ELSE (
+    %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 )
-
-if "%1" == "" goto help
-
-%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
-goto end
+GOTO end
 
 :help
 %SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
