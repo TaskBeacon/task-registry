f36474f06b6be63a6111a75448bef1cc140964e4
zh1peng <frueddy@gmail.com>
2025-06-13 08:44:08 +0800

    init furo

---
+++ build/html/_sources/index.rst.txt
@@ -0,0 +1,20 @@
+.. task_index documentation master file, created by
+   sphinx-quickstart on Fri Jun 13 08:40:09 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to task_index's documentation!
+======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ source/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'task_index'
+copyright = '2025, zhipeng'
+author = 'zhipeng'
+release = '1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ['_static']
+
+html_logo = "_static/logo.png"  # add later if you want
+html_theme_options = {
+    "sidebar_hide_name": True,
+    "light_logo": "logo-light.png",
+    "dark_logo": "logo-dark.png",
+}
